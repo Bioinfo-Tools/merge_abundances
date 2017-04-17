774241e8876f0f4366ae516d953f17347c0f1226
raquel <raquel.dias@ufl.edu>
2017-04-17 16:08:32 -0700

    uploading script

---
+++ merge_abundances.R
@@ -0,0 +1,22 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+print(args)
+
+merged_table <- read.table(args[1], header=TRUE, sep="\t")
+length(args)
+
+for(i in 2:length(args)){
+
+    new_table <- read.table(args[i], header=TRUE, sep="\t")
+    label<-as.character(names(merged_table)[1])
+    #print(label)
+    #print(head(merged_table))
+    #print(head(new_table))
+
+    merged_table <- merge(merged_table, new_table, by=label, all=TRUE)
+    #head(merged_table)
+
+}
+merged_table[is.na(merged_table)] <- 0
+names(merged_table)[2:(length(args)+1)] <- args
+print(merged_table)
